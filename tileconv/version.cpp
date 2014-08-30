1f9d74cd227c0b1ae3ca8265021c311de377cdad
Argent77 <argent77@server.fake>
2014-08-30 14:11:44 +0200

    Version 0.1

--- tileconv/version.cpp
+++ tileconv/version.cpp
@@ -19,7 +19,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-int vers_major = 1;
-int vers_minor = 0;
+int vers_major = 0;
+int vers_minor = 1;
 char prog_name[] = "tileconv";
 char author[] = "Argent77";
